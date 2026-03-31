5489f19cfa95fdd4caa42790d21cb39720d49519
Deepu <dy107817@gmail.com>
2026-03-31 18:08:46 +0530

    Updated in the Enum Class

--- ATMMACHINE/Enum/ATMStatus.java
+++ ATMMACHINE/Enum/ATMStatus.java
@@ -1,8 +1,10 @@
 package ATMMACHINE.Enum;
 
+// this is enum so you have to keep track everything like this.
 public enum ATMStatus {
      IDLE,
     CARD_INSERTED,
     AUTHENTICATED,
     CASH_DISPENSE
+
 }
